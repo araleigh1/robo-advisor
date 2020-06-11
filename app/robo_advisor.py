0ab92bb866b2428f09ac98f1075131a89fac23ec
araleigh1 <65408874+araleigh1@users.noreply.github.com>
2020-06-10 22:54:29 -0400

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,6 +1,9 @@
-import requests
+import csv
+import os
 import json
 import datetime
+
+import requests
 
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
@@ -39,13 +42,28 @@ recent_high = max(high_prices)
 recent_low = min(low_prices)
 
 
+csv_file_path = os.path.join(os.path.dirname(__file__),"..", "data", "prices.csv")
+csv_headers =["timestamp", "open", "high", "low", "close", "volume"]
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader() # uses fieldnames set above
+    for date in dates:
+        daily_prices = tsd[date]
+        writer.writerow({
+        "timestamp": date,
+        "open": daily_prices["1. open"],
+        "high": daily_prices["2. high"],
+        "low": daily_prices["3. low"],
+        "close": daily_prices["4. close"],
+        "volume": daily_prices["5. volume"]})
 
 #
 #
 #
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: MSFT")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print(f"REQUEST AT: " + today.strftime("%Y-%m-%d %I:%M %p"))
