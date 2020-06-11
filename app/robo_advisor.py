ac373306750566c9b3865c352b7d955dc6ca52ba
araleigh1 <65408874+araleigh1@users.noreply.github.com>
2020-06-10 21:57:50 -0400

    Added dynamic latest day

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -5,6 +5,9 @@ import datetime
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
+today = datetime.datetime.now()
+
+
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 response = requests.get(request_url)
 #print(type(response))
@@ -12,10 +15,17 @@ response = requests.get(request_url)
 #print(response.text)
 
 parsed_response = json.loads(response.text)
-print(parsed_response.keys())
+
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-latest_close = parsed_response["Time Series (Daily)"]["2020-06-10"]["4. close"]
+
+tsd = parsed_response["Time Series (Daily)"]
+dates = list(tsd.keys())
+
+latest_day =dates[0]
+
+latest_close = parsed_response["Time Series (Daily)"][latest_day]["4. close"]
 print(latest_close)
+
 
 
 
@@ -28,7 +38,7 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST AT: " + today.strftime("%Y-%m-%d %I:%M %p"))
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
